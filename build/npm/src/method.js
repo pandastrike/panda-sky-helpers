96a2c837a4efb43c8df687b237f119076deac1a8
David Harper <real.david.harper@gmail.com>
2018-07-29 18:24:06 -0700

    Added support for the detection of CloudWatch scheduled events from the new Cuddle Monkey mixin.  This provides a point in the request-response cycle to catch all CloudWatch invocations and short-circuit the reply.

--- build/npm/src/method.js
+++ build/npm/src/method.js
@@ -6,13 +6,24 @@ Object.defineProperty(exports, "__esModule", {
 
 var _pandaAuthHeader = require("panda-auth-header");
 
+var _logger = require("./logger");
+
+var _logger2 = _interopRequireDefault(_logger);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
 var method;
 
 method = function (handler) {
   // TODO: parse Accept header
   // TODO: logging instrumentation
+  // TODO: Allow the reaction to CloudWatch events to default to short-circuiting with an optional override based on extracting a special handler from each API file.
   return function (request, context) {
     var header, params, ref, scheme, token;
+    if (request.source === "cuddle-monkey") {
+      _logger2.default.info("Detected a Cuddle Monkey preheater invocation. Short circuting request cycle.");
+      return true;
+    }
     if ((header = (ref = request.headers) != null ? ref['Authorization'] : void 0) != null) {
       ({ scheme, params, token } = (0, _pandaAuthHeader.parse)(header));
       if (token) {
--- src/method.coffee
+++ src/method.coffee
@@ -1,9 +1,16 @@
 import {parse} from "panda-auth-header"
+import log from "./logger"
 
 method = (handler) ->
   # TODO: parse Accept header
   # TODO: logging instrumentation
+  # TODO: Allow the reaction to CloudWatch events to default to short-circuiting with an optional override based on extracting a special handler from each API file.
+
   (request, context) ->
+    if request.source == "cuddle-monkey"
+      log.info "Detected a Cuddle Monkey preheater invocation. Short circuting request cycle."
+      return true
+
     if (header = request.headers?['Authorization'])?
       {scheme, params, token} = parse header
       if token
